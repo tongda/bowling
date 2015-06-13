b506ac4d3d6320c8b5246cdeac7e0f50eeb564a8
Tong Damon Da <tongda@outlook.com>
2015-06-13 14:02:32 +0800

    refactor: remove conditional expressions.

--- bowling/round.py
+++ bowling/round.py
@@ -5,6 +5,7 @@ SPARE = 1
 COUNT_SELF = 2
 NON_START = -1
 
+
 class Round(object):
     def __init__(self, hit_num=2):
         self.state = NON_START
@@ -14,16 +15,17 @@ class Round(object):
 
     @property
     def score(self):
-        return sum(self.hit_scores)
+        return sum(map(lambda h: h.score, self.hits), 0)
 
     def hit(self, score):
+        conds = {
+            (0, 10): lambda: Hit(score=score, count_next=2),
+            (1, 10): lambda: Hit(score=score, count_next=1),
+        }
 
-        if self.hit_times == 0 and score == 10:
-            self.hits.append(Hit(score=score, count_next=2))
-        elif self.hit_times == 1 and self.hits[0].score + score == 10:
-            self.hits.append(Hit(score=score, count_next=1))
-        else:
-            self.hits.append(Hit(score=score, count_next=0))
+        self.hits.append(
+            conds.get((self.hit_times, self.score + score),
+                      lambda: Hit(score=score, count_next=0))())
 
         self.hit_times += 1
 
@@ -33,12 +35,7 @@ class FinalRound(Round):
         super().__init__(hit_num=3)
 
     def hit(self, score):
-        if self.hit_times == 0 and score == 10:
-            self.hits.append(Hit(score=score, count_next=0))
-        elif self.hit_times == 1 and self.hits[0].score + score == 10:
-            self.hits.append(Hit(score=score, count_next=0))
-        else:
-            self.hits.append(Hit(score=score, count_next=0))
+        self.hits.append(Hit(score=score, count_next=0))
 
         self.state = COUNT_SELF
         self.hit_times += 1
