06d87d076422b2f656c3c914f4f570d4ce506107
Tong Damon Da <tongda@outlook.com>
2015-06-13 15:16:54 +0800

    refactor: remove loop

--- bowling/game.py
+++ bowling/game.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import itertools
 
 from .round import Round
@@ -6,7 +7,7 @@ from .round import FinalRound
 
 class Game(object):
     def __init__(self, round_num):
-        self.rounds = [Round() for _ in range(round_num - 1)]
+        self.rounds = list(map(lambda _: Round(), range(round_num - 1)))
         self.rounds.append(FinalRound())
 
     def score(self):
@@ -17,5 +18,6 @@ class Game(object):
             return hs[0].score + sum(
                 map(lambda h: h.score, hs[1:1 + hs[0].count_next]))
 
-        return sum([calc_score_new(hs) for hs in
-                    itertools.zip_longest(hits, hits[1:], hits[2:])], 0)
+        return reduce(lambda sum, hs: sum + calc_score_new(hs),
+                      itertools.zip_longest(hits, hits[1:], hits[2:]),
+                      0)
